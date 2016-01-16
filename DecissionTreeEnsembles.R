87c32292e18db570df6186bb448720fdcf275953
hemal-thakkar <hemal.tt@gmail.com>
2016-01-15 22:52:48 -0700

    Format change and consolidate final result

--- DecissionTreeEnsembles.R
+++ DecissionTreeEnsembles.R
@@ -1,11 +1,11 @@
 # Script To use the following Decission Tree and Ensemble techniques for simple predictive analytics
-# Input: dataSet: R data frame, Predictive analytics applied to first column using the remaining ones
-# Output list('Technique'=RMSEAccuracy)
+# The script is intentionally kept simple to cover the basics of using simple decission tree ensemble techniques
 
 # Play with data
 ?mtcars
 mtcars
 plot(mtcars$mpg,mtcars$carb)
+finalResult = list()
 
 #Sample the data set
 smp_size <- floor(0.75 * nrow(mtcars))
@@ -18,6 +18,8 @@ test_orig <- test$mpg
 f <- as.formula(paste("mpg ~ ",paste(names(train)[-1],collapse = " + ")))
 library(rpart)
 
+
+#####################################################################################
 #build a simple decission tree model
 carfit_dt <- rpart(f, data = train, method = "anova")
 
@@ -27,8 +29,9 @@ test_pred_dt <- predict(carfit_dt, test[,-1])
 #Calculate accuracy
 med <- median(test_orig)
 RMSE_dt <- sqrt(mean((test_orig-test_pred_dt)^2))
-cat("One Decission True : ", (1-RMSE_dt/med))
+finalResult$DT <- (1-RMSE_dt/med)
 
+#####################################################################################
 #build a random forest model
 set.seed(415)
 install.packages('randomForest')
@@ -49,7 +52,5 @@ med <- median(test_orig)
 med <- mean(test_orig)
 RMSE_rnd <- sqrt(mean((test_orig-test_pred_rnd)^2))
 RMSE_rnd
-1-RMSE_rnd/med
-plot(test_orig-test_pred_rnd)
-names(test_orig) <- names (test_pred_rnd)
-
+finalResult$Rnd_For <- 1-RMSE_rnd/med
+#####################################################################################
